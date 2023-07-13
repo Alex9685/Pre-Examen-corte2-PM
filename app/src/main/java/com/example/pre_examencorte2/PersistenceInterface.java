2349783711e351ffc03c7efdbc55b3d9bc4ca61c
trikitrakatelas <alex928280@gmail.com>
2023-07-12 17:39:44 -0700

    Iterfaces y terminacion de las actividades

--- app/src/main/java/com/example/pre_examencorte2/PersistenceInterface.java
+++ app/src/main/java/com/example/pre_examencorte2/PersistenceInterface.java
@@ -1,2 +1,6 @@
 package com.example.pre_examencorte2;
 
+public interface PersistenceInterface {
+    void createTable();
+    void insert(Object object);
+}
--- app/src/main/java/com/example/pre_examencorte2/ProjectionInterface.java
+++ app/src/main/java/com/example/pre_examencorte2/ProjectionInterface.java
@@ -1,1 +1,10 @@
 package com.example.pre_examencorte2;
+
+import android.database.Cursor;
+
+import java.util.List;
+
+public interface ProjectionInterface {
+    List<Usuario> allUsuarios();
+    Usuario readUsuario(Cursor cursor);
+}
